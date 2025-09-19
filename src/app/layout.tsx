5ce0808ea3c528b9a39396d562f0daa88e0dc6c5
chenrun666 <7417245+chenrun666@user.noreply.gitee.com>
2025-09-19 17:59:19 +0800

    feat: Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { homePath, loginPath, ticketsPath } from "@/path";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { LucideKanban } from "lucide-react";
+import { Header } from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,42 +32,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <nav
-          className="
-          supports-backdrop-blur:bg-background/60
-          fixed left-0 right-0 top-0 z-20
-          border-b bg-baackground/95 backdrop-blur
-          w-full flex justify-between py-2.5 px-5"
-        >
-          <div className="flex gap-2">
-            <div>
-              <Button asChild variant="outline">
-                <Link href={homePath}>
-                  <LucideKanban />
-                  <h1 className="text-lg font-semibold">TicketBounty</h1>
-                </Link>
-              </Button>
-            </div>
-            <div>
-              <Link
-                href={ticketsPath}
-                className={buttonVariants({ variant: "outline" })}
-              >
-                Tickets
-              </Link>
-            </div>
-          </div>
-          <div className="flex gap-2">
-            <div>
-              <Link
-                href={loginPath}
-                className={buttonVariants({ variant: "outline" })}
-              >
-                Login
-              </Link>
-            </div>
-          </div>
-        </nav>
+        <Header />
+
         <main
           className="
           main-h-screen flex-1
