fa0cbbbef77a8c99f70b05ec050ba0aecf498bf0
chenrun666 <7417245+chenrun666@user.noreply.gitee.com>
2025-09-19 22:31:08 +0800

    feat: Modify, add right side bar with ticket card

--- src/features/tickets/components/ticket-item.tsx
+++ src/features/tickets/components/ticket-item.tsx
@@ -10,33 +10,42 @@ import { ticketPath } from "@/path";
 import clsx from "clsx";
 import Link from "next/link";
 import { TicketProps } from "../types";
+import { LucideSquareArrowOutUpRight, LucideTrash2 } from "lucide-react";
 
 const TicketItem = (ticketProps: TicketProps) => {
   const ticket = ticketProps.ticket;
 
   return (
-    <Card key={ticket.id} className="w-full max-w-[420px] mb-4">
-      <CardHeader>
-        <CardTitle className="flex flex-row justify-between items-center">
-          <span className="line-clamp-1 w-full max-w-[250]">
-            {ticket.title}
-          </span>
-          <span>{TicketStatus[ticket.status]}</span>
-        </CardTitle>
-      </CardHeader>
-      <CardContent
-        className={clsx("text-sm line-clamp-1 w-full max-w-[350]", {
-          "line-through text-gray-400": ticket.status === "CLOSED",
-        })}
-      >
-        {ticket.description}
-      </CardContent>
-      <CardFooter className="justify-end">
+    <div className="flex flex-row w-full max-w-[420px] mb-4">
+      {/* Ticket Card Info */}
+      <Card key={ticket.id} className="w-full mr-2">
+        <CardHeader>
+          <CardTitle className="flex flex-row justify-between items-center">
+            <span className="line-clamp-1 w-full max-w-[250]">
+              {ticket.title}
+            </span>
+            <span>{TicketStatus[ticket.status]}</span>
+          </CardTitle>
+        </CardHeader>
+        <CardContent
+          className={clsx("text-sm line-clamp-1 w-full max-w-[350]", {
+            "line-through text-gray-400": ticket.status === "CLOSED",
+          })}
+        >
+          {ticket.description}
+        </CardContent>
+      </Card>
+
+      {/* right side bar button */}
+      <div className="flex flex-col justify-between items-center max-h-[50px] mt-2">
         <Link href={ticketPath(ticket.id)} className="underline text-sm">
-          View
+          <LucideSquareArrowOutUpRight className="size-3.5"/>
         </Link>
-      </CardFooter>
-    </Card>
+        <Link href={ticketPath(ticket.id)} className="underline text-sm">
+          <LucideTrash2 className="size-3.5"/>
+        </Link>
+      </div>
+    </div>
   );
 };
 
