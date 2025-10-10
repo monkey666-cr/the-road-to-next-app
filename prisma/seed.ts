e3088ee14465d90c7109472b1d4e7c688f7fabef
chenrun666 <7417245+chenrun666@user.noreply.gitee.com>
2025-10-10 22:27:29 +0800

    feat: add ticket, user relation

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,20 @@
+import { hash } from "@node-rs/argon2";
 import { PrismaClient, Ticket, TicketStatus } from "@prisma/client";
 import { format } from "date-fns";
+import { email } from "zod";
 
 const prisma = new PrismaClient();
+
+const users = [
+  {
+    username: "admin",
+    email: "admin@admin.com",
+  },
+  {
+    username: "user",
+    email: "hello@road-to-next.com",
+  },
+];
 
 const tickets: Ticket[] = [
   {
@@ -13,6 +26,7 @@ const tickets: Ticket[] = [
     bounty: 0,
     createdAt: new Date(),
     updatedAt: new Date(),
+    userId: "", // Placeholder, will be set during seeding
   },
 ];
 
@@ -20,13 +34,20 @@ const seed = async () => {
   const t0 = performance.now();
   console.log("Seeding database...");
 
-  for (const ticket of tickets) {
-    await prisma.ticket.upsert({
-      where: { id: ticket.id },
-      create: ticket,
-      update: ticket,
-    });
-  }
+  await prisma.user.deleteMany();
+  await prisma.ticket.deleteMany();
+
+  const passwordHash = await hash("password123");
+  const dbUsers = await prisma.user.createManyAndReturn({
+    data: users.map((user) => ({ ...user, passwordHash })),
+  });
+
+  await prisma.ticket.createMany({
+    data: tickets.map((ticket) => ({
+      ...ticket,
+      userId: dbUsers[0].id, // Assigning all tickets to the second user
+    })),
+  });
 
   console.log("Seeding completed.");
   console.log(`Time taken: ${(performance.now() - t0).toFixed(2)} ms`);
